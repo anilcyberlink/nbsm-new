f604b7d94f58d7142550ce82353547c410bb49d7
anilcyberlink <anilcyberlink@gmail.com>
2026-08-26 11:36:53 +0545

    Footer development

--- app/Http/ViewComposers/FooterComposer.php
+++ app/Http/ViewComposers/FooterComposer.php
@@ -16,12 +16,17 @@ class FooterComposer{
 
 	public function compose(View $view){
 
-		$view->with('footeer', PostTypeModel::where(['is_footer_menu'=>'1'])
+		$view->with('footer', PostTypeModel::where(['is_footer_menu'=>'1'])
 			->orderBy('ordering','asc')
 			->get());
 
-		$view->with('services',PostModel::where(['post_category'=>'3'])
-				->get());
+		$view->with('service',PostTypeModel::where(['id'=>'16'])->first());
+
+		$view->with('services',PostModel::where(['post_type'=>'16','post_parent'=>'0'])->get());
+
+		$view->with('industry',PostTypeModel::where(['id'=>'15'])->first());
+
+		$view->with('industries',PostModel::where(['post_type'=>'15','post_parent'=>'0'])->take(6)->get());
 
 		$view->with('quick_links',PostModel::where(['post_category'=>'2'])
 				->get());
